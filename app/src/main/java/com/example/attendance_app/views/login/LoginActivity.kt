b53a7ac26d6c68d4d6a5e0d11110e08cb09939bf
= <bbhpowpow@gmail.com>
2022-03-09 14:57:07 +0700

    ui: add login page

---
+++ app/src/main/java/com/example/attendance_app/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.attendance_app.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.attendance_app.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
